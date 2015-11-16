02ab589e1f8f7011a3fc574dd30250fdc49c2a08
root <root@flarotag1.mylabserver.com>
2015-11-16 18:39:29 +0000

    Commit chef-repo for the first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,11 @@
+default["apache"]["sites"]["flarotag2"] = { "site_title" => "Flarotag2s Website coming soon", "port" => 80, "domain" => "flarotag2.mylabserver.com" }
+default["apache"]["sites"]["flarotag2b"] = { "site_title" => "Flarotag2bs Website is coming soon!", "port" => 80, "domain" => "flarotag2b.mylabserver.com" }
+default["apache"]["sites"]["flarotag4"] = { "site_title" => "flarotag4 website", "port" => 80, "domain" => "flarotag4.mylabserver.com" }
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
+  
