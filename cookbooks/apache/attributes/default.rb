a8c70712c305f23f25ee7dc171fb7bb3d79889c6
root <root@flarotag1.mylabserver.com>
2015-11-24 14:13:16 +0000

    updating out worksstation chef-repo

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["flarotag2"] = { "site_title" => "Flarotag2s Website 
 default["apache"]["sites"]["flarotag2b"] = { "site_title" => "Flarotag2bs Website is coming soon!", "port" => 80, "domain" => "flarotag2b.mylabserver.com" }
 default["apache"]["sites"]["flarotag4"] = { "site_title" => "flarotag4 website", "port" => 80, "domain" => "flarotag4.mylabserver.com" }
 
+default["author"]["name"] = "Paul"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
